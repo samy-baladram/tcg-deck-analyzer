61f4fce31eb72622a8fa87d874a9e4ce3a7f6d2e
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-05-15 00:53:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -297,7 +297,7 @@ BASE_URL = "https://play.limitlesstcg.com"
 
 # Main title
 #st.title("Pokémon TCG Pocket Meta Deck Analyzer")
-st.image("title_img.png", use_column_width=True)
+st.image("title_banner.png", use_container_width=True)
 
 # Initialize session state and fetch deck list on first load
 if 'deck_list' not in st.session_state:
