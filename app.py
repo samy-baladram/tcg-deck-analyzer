baad5ce0d266eaea665dab8145181a87a1e78125
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-05-15 15:19:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -468,7 +468,7 @@ if 'analyze' in st.session_state and selected_option:
     img_base64 = get_base64_image("solgaleo_sample.png")
     st.markdown(f"""
     <div style="display: flex; align-items: center; margin-bottom: 0rem;">
-        <img src="data:image/png;base64,{img_base64}" style="height: 6em; margin-right: 0.5em; margin-bottom:-2em;"><\br>
+        <img src="data:image/png;base64,{img_base64}" style="height: 6em; margin-right: 0.5em; margin-bottom:-2em;"><br>
         <h3 style="margin: 0;">{format_deck_name(deck_info['deck_name'])}</h3>
     </div>
     """, unsafe_allow_html=True)
