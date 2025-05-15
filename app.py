d874992deb4d1a9adf096f7e354eb24322b25225
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-05-15 13:57:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -454,7 +454,7 @@ if 'analyze' in st.session_state and selected_option:
     
     with tab1:
         #st.subheader(f"Card Usage Summary")
-        st.markdown('<p style="color: #70a1e2;">C A R D&ensp;U S A G E&ensp;S U M M A R Y</p>', unsafe_allow_html=True)
+        st.markdown('<h3 style="color: #70a1e2;">C A R D&ensp;&ensp;U S A G E&ensp;&ensp;S U M M A R Y</h3>', unsafe_allow_html=True)
         #st.write("C A R D&ensp;U S A G E&ensp;S U M M A R Y") 
         # Create two columns for Pokemon and Trainer
         col1, col2 = st.columns(2)
