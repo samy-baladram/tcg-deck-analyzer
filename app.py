f25915b997bc99820e934b1902466140ba00c674
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-05-15 18:36:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ if 'analyze' in st.session_state and selected_option:
     if header_images:
         st.markdown(f"""
         <div style="display: flex; align-items: center; margin-bottom: 0rem;">
-            <img src="data:image/png;base64,{header_images[0]}" style="width: 49.5%; max-width: 300px; height: auto; margin-right: -19.5%; margin-bottom:-2.3em;">
+            <img src="data:image/png;base64,{header_images[0]}" style="width: 49.5%; max-width: 300px; height: auto; margin-right: -9.5%; margin-bottom:-2.3em;">
             <img src="data:image/png;base64,{header_images[1]}" style="width: 49.5%; max-width: 300px; height: auto; margin-right: 0em; margin-bottom:-2.3em;">
         </div>
         <div style="display: flex; align-items: center; margin-bottom: -2rem;">
