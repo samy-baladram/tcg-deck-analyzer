35a69d9ba5a14f366c65800cf1097da328f11785
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-05-19 01:08:31 +0900

    Update image_processor.py

--- image_processor.py
+++ image_processor.py
@@ -8,9 +8,9 @@ from io import BytesIO
 import re
 from config import IMAGE_BASE_URL, IMAGE_CROP_BOX, IMAGE_GRADIENT
 from utils import is_set_code
-GAP_RATIO = -0.12
+GAP_RATIO = -0.25
 EDGE_CUTOFF = 0.04
-GRADIENT_RATIO = 0.04 
+GRADIENT_RATIO = 0.2 
 
 # Simple utility functions
 def get_base64_image(path):
