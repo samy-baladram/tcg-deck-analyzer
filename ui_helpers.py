c62ea46acefb094cf3090d1134ff7b4585952b46
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-05-19 04:34:22 +0900

    Update ui_helpers.py

--- ui_helpers.py
+++ ui_helpers.py
@@ -229,8 +229,8 @@ def render_sidebar():
         with open(banner_path, "rb") as f:
             banner_base64 = base64.b64encode(f.read()).decode()
         st.sidebar.markdown(f"""
-        <div style="width:100%; text-align:left; margin:-20px 0 5px 0;">
-            <img src="data:image/png;base64,{banner_base64}" style="width:100%; max-width:350px;">
+        <div style="width:100%; text-align:center; margin:-20px 0 5px 0;">
+            <img src="data:image/png;base64,{banner_base64}" style="width:100%; max-width:350px; margin-bottom:10px;">
         </div>
         """, unsafe_allow_html=True)
     else:
