bae180382f49652628b60afb71d426f76667b8ae
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-05-19 16:29:16 +0900

    Update image_processor.py

--- image_processor.py
+++ image_processor.py
@@ -9,7 +9,7 @@ from io import BytesIO
 import re
 from config import IMAGE_BASE_URL, IMAGE_CROP_BOX, IMAGE_GRADIENT
 from utils import is_set_code
-GAP_RATIO = -0.1
+GAP_RATIO = -0.05
 EDGE_CUTOFF = 0.02
 GRADIENT_RATIO = 0.15
 
