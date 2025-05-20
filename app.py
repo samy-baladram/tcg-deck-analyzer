3ac3c3acf5f887538af523070a806793dbc2c375
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-05-20 23:55:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -159,7 +159,7 @@ if 'analyze' in st.session_state and selected_option:
     display_tabs.display_deck_header(original_deck_info, results)
     
     # Create tab container
-    tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Deck Template", 
+    tab1, tab2, tab3, tab4, tab5, tab6,tab7 = st.tabs(["Deck Template", 
                                                         "Card Usage",  
                                                         "Meta Matchups",
                                                         "Metagame Overview",
