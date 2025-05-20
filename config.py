a73aa1b1905e76829ddb5800ef524cb3af640c1a
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-05-20 17:39:34 +0900

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ BASE_URL = "https://play.limitlesstcg.com"
 IMAGE_BASE_URL = "https://limitlesstcg.nyc3.cdn.digitaloceanspaces.com/pocket"
 
 # Cache settings
-CACHE_TTL = 10  # 1 hour in seconds
+CACHE_TTL = 30  # 1 hour in seconds
 
 # Tournament count
 TOURNAMENT_COUNT = 70
