e01a8e05ae14fa63fceb781d1075272d289693a3
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-05-22 17:23:51 +0900

    Update display_tabs.py

--- display_tabs.py
+++ display_tabs.py
@@ -38,7 +38,7 @@ def display_deck_header(deck_info, results):
         header_content = f"""
         <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@1,900&display=swap" rel="stylesheet">
         <div style="display: flex; flex-wrap: wrap; align-items: center; margin-bottom: 1em; margin-top:0.25rem">
-            <div style="margin-right: 0rem; margin-left:0rem; margin-bottom: -0.8rem;">
+            <div style="margin-right: 0rem; margin-left:0rem; margin-bottom: -1rem;">
                 <img src="data:image/png;base64,{header_image}" style="width: 100%; max-width: 350px; height: auto; border-radius: 10px;">
             </div>"""
         
@@ -61,7 +61,7 @@ def display_deck_header(deck_info, results):
         
         # Add the deck name
         header_content += f"""
-                <h2 style="margin-bottom:-2rem; font-family: 'Nunito', sans-serif; font-weight: 900; font-style: italic; line-height: 1.2; word-wrap: break-word;">{format_deck_name(deck_info['deck_name'])}</h2>
+                <h2 style="margin-bottom:-1.5rem; font-family: 'Nunito', sans-serif; font-weight: 900; font-style: italic; line-height: 1.2; word-wrap: break-word;">{format_deck_name(deck_info['deck_name'])}</h2>
             </div>
         </div>"""
         
