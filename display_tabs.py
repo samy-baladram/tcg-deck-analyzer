367a09dd8105f242c6f052af0a145f795aff56b4
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-05-22 15:32:48 +0900

    Update display_tabs.py

--- display_tabs.py
+++ display_tabs.py
@@ -37,7 +37,7 @@ def display_deck_header(deck_info, results):
         # Build the header content with conditional featured image
         header_content = f"""
         <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@1,900&display=swap" rel="stylesheet">
-        <div style="display: flex; flex-wrap: wrap; align-items: center; margin-bottom: 0.75rem; margin-top:0.25rem">
+        <div style="display: flex; flex-wrap: wrap; align-items: center; margin-bottom: 0.25rem; margin-top:0.25rem">
             <div style="margin-right: 1rem; margin-bottom: -20px;">
                 <img src="data:image/png;base64,{header_image}" style="width: 100%; max-width: 400px; height: auto; border-radius: 10px;">
             </div>
