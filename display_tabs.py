7a26103408a362de6f475bfbbd5890b5afc1a36f
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-05-22 18:03:11 +0900

    Update display_tabs.py

--- display_tabs.py
+++ display_tabs.py
@@ -37,7 +37,7 @@ def display_deck_header(deck_info, results):
         # Build the header content with conditional featured image
         header_content = f"""
         <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@1,900&display=swap" rel="stylesheet">
-        <div style="display: flex; flex-wrap: wrap; align-items: center; margin-bottom: 1em; margin-top:0.25rem">
+        <div style="display: flex; flex-wrap: wrap; align-items: center; margin-bottom: 0.5em; margin-top:0.25rem">
             <div style="margin-right: 0rem; margin-left:0rem; margin-bottom: -1rem;">
                 <img src="data:image/png;base64,{header_image}" style="width: 100%; max-width: 350px; height: auto; border-radius: 10px;">
             </div>"""
@@ -46,7 +46,7 @@ def display_deck_header(deck_info, results):
         if show_landing_message and featured_image_base64:
             header_content = f"""
             <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@1,900&display=swap" rel="stylesheet">
-            <div style="display: flex; flex-wrap: wrap; align-items: center; margin-bottom: -1em; margin-top:0rem">
+            <div style="display: flex; flex-wrap: wrap; align-items: center; margin-bottom: 1em; margin-top:0rem">
                 <div style="margin-right: 0rem; margin-left:0rem; margin-bottom: -1rem;">
                     <img src="data:image/png;base64,{header_image}" style="width: 100%; max-width: 450px; height: auto; border-radius: 10px;">
                 </div>"""
