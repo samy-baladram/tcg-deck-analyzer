3b5193646b17aee4c7af846c02103dd7f3a3f486
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-05-22 17:46:13 +0900

    Update display_tabs.py

--- display_tabs.py
+++ display_tabs.py
@@ -38,7 +38,7 @@ def display_deck_header(deck_info, results):
         header_content = f"""
         <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@1,900&display=swap" rel="stylesheet">
         <div style="display: flex; flex-wrap: wrap; align-items: center; margin-bottom: 1em; margin-top:0.25rem">
-            <div style="margin-right: 0rem; margin-left:0rem; text-align: center; margin-bottom: -1rem;">
+            <div style="margin-right: 0rem; margin-left:0rem; margin-bottom: -1rem;">
                 <img src="data:image/png;base64,{header_image}" style="width: 100%; max-width: 350px; height: auto; border-radius: 10px;">
             </div>"""
         
