c38a8b9d6113a40280d9fcdadfcb270e78258e10
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-05-24 18:06:41 +0900

    Update ui_helpers.py

--- ui_helpers.py
+++ ui_helpers.py
@@ -630,7 +630,7 @@ def render_sidebar_from_cache():
             with open(trending_banner_path, "rb") as f:
                 trending_banner_base64 = base64.b64encode(f.read()).decode()
             st.markdown(f"""
-            <div style="width:100%; text-align:center;">
+            <div style="width:100%; text-align:center; margin-top:-40px;">
                 <hr style='margin-bottom:15px;  border: 0.5px solid #aaa;'>
                 <img src="data:image/png;base64,{trending_banner_base64}" style="width:100%; max-width:350px;">
             </div>
@@ -810,7 +810,7 @@ def display_counter_picker_sidebar():
         with open(banner_path, "rb") as f:
             banner_base64 = base64.b64encode(f.read()).decode()
         st.markdown(f"""
-        <div style="width:100%; text-align:center; margin-top:-30px;">
+        <div style="width:100%; text-align:center; margin-top:-40px;">
                 <hr style='margin-bottom:15px; border: 0.5px solid #aaa;'>
             <img src="data:image/png;base64,{banner_base64}" style="width:100%; max-width:350px; margin-bottom:10px;">
         </div>
