9090f945a39e7bd4fa0bb73c0a11d60ea6c1ffcb
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-05-24 18:14:30 +0900

    Update ui_helpers.py

--- ui_helpers.py
+++ ui_helpers.py
@@ -809,9 +809,8 @@ def display_counter_picker_sidebar():
     if os.path.exists(banner_path):
         with open(banner_path, "rb") as f:
             banner_base64 = base64.b64encode(f.read()).decode()
-        st.markdown("<hr style='margin:20px;  border: 0.5px solid #aaa;'>", unsafe_allow_html=True)    
-        st.markdown(f"""
         <div style="width:100%; text-align:center;">
+            <hr style='margin-bottom:20px;  border: 0.5px solid #aaa; margin-top:-30px;'>
             <img src="data:image/png;base64,{banner_base64}" style="width:100%; max-width:350px; margin-bottom:10px;">
         </div>
         """, unsafe_allow_html=True)
