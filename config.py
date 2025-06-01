35c99e605f22ba46077a5fca13ea9a2579e96438
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-06-01 12:48:09 +0900

    Update config.py

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ IMAGE_BASE_URL = "https://limitlesstcg.nyc3.cdn.digitaloceanspaces.com/pocket"
 
 # Cache settings
 CACHE_TTL = 3600  # 1 hour in seconds
+CURRENT_SET = "A3a"
 
 # Add to config.py
 ALGORITHM_VERSION = 1  # Increment this when you change algorithms like analyze_recent_performance
