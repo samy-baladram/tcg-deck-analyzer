6a6d95399dde0e0ea0d7effbacb4581d5415c0e4
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-06-08 16:26:17 +0900

    Update ui_helpers.py

--- ui_helpers.py
+++ ui_helpers.py
@@ -887,7 +887,7 @@ def create_deck_section(section_type):
         
     if header_image:
         st.markdown(f"""
-        <div style="width: 100%; margin-top: -18px; margin-bottom: -12px; position: relative;">
+        <div style="width: 100%; margin-top: -18px; margin-bottom: -5px; position: relative;">
             <img src="data:image/png;base64,{header_image}" style="width: 100%; height: auto; border-radius: 4px; z-index:-2;">
             <div style="position: absolute; bottom: 0px; right: 0px; background-color: rgba(0, 0, 0, 0.8); color: white; padding: 2px 4px; border-radius: 4px 0px 4px 0px; font-size: 0.6rem; font-weight: 700;">
                 {stats_text}
