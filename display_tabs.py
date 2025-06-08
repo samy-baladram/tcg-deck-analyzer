988adbd48fb6c64bc303bc4411d2f32ab48b9670
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-06-08 12:49:44 +0900

    Update display_tabs.py

--- display_tabs.py
+++ display_tabs.py
@@ -44,7 +44,7 @@ def display_deck_header(deck_info, results):
         header_content = f"""
         <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@1,900&display=swap" rel="stylesheet">
         <div style="display: flex; flex-wrap: wrap; justify-content: center; align-items: center; gap: 0rem; margin: 0rem 0 0.5rem 0; text-align: center;">
-            <div style="min-width: 200px; margin-right: 1rem;">"""
+            <div style="min-width: 200px; ">"""
         
         # Add featured image if this is the first time and image exists
         if show_landing_message and featured_image_base64:
