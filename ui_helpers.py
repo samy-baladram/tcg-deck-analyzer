8771f915380d6f411b15347b94399315507143d8
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-06-08 12:03:13 +0900

    Update ui_helpers.py

--- ui_helpers.py
+++ ui_helpers.py
@@ -823,7 +823,7 @@ def create_deck_section(section_type):
     if os.path.exists(config['banner_path']):
         banner_base64 = get_cached_banner_image(config['banner_path'])
         if banner_base64:
-            st.markdown(f"""<div style="width:100%; text-align:center;">
+            st.markdown(f"""<div style="width:100%; text-align:center; margin-bottom:-10px;">
                 <hr style='margin-bottom:10px; border: 0.5px solid rgba(137, 148, 166, 0.3); margin-top:0px;'>
                 <img src="data:image/png;base64,{banner_base64}" style="width:100%; max-width:350px;">
             </div>
