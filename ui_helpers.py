76c77d8f46479c7a427cb2bde44ca1384edd9516
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-06-08 12:01:50 +0900

    Update ui_helpers.py

--- ui_helpers.py
+++ ui_helpers.py
@@ -824,7 +824,7 @@ def create_deck_section(section_type):
         banner_base64 = get_cached_banner_image(config['banner_path'])
         if banner_base64:
             st.markdown(f"""<div style="width:100%; text-align:center;">
-                <hr style='margin-bottom:10px; border: 0.5px solid rgba(137, 148, 166, 0.2); margin-top:0px;'>
+                <hr style='margin-bottom:10px; border: 0.5px solid rgba(137, 148, 166, 0.3); margin-top:0px;'>
                 <img src="data:image/png;base64,{banner_base64}" style="width:100%; max-width:350px;">
             </div>
             """, unsafe_allow_html=True)
@@ -1103,7 +1103,7 @@ def display_counter_picker_sidebar():
         banner_base64 = get_cached_banner_image(config['banner_path'])
         if banner_base64:
             st.markdown(f"""<div style="width:100%; text-align:center;">
-                <hr style='margin-bottom:20px; border: 0.5px solid rgba(137, 148, 166, 0.2); margin-top:-25px;'>
+                <hr style='margin-bottom:10px; border: 0.5px solid rgba(137, 148, 166, 0.3); margin-top:0px;'>
                 <img src="data:image/png;base64,{banner_base64}" style="width:100%; max-width:350px; margin-bottom:10px;">
             </div>
             """, unsafe_allow_html=True)
