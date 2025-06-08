efdc430052d9f74254d21b1f159df49d16450560
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-06-08 16:09:17 +0900

    Update ui_helpers.py

--- ui_helpers.py
+++ ui_helpers.py
@@ -817,7 +817,7 @@ def render_unified_deck_in_sidebar(deck, section_config, rank=None, expanded=Fal
         header_image = get_header_image_cached(deck['deck_name'], deck['set'])
         if header_image:
             st.markdown(f"""
-            <div style="width: 100%; margin-top: -18px; margin-bottom: 12px; position: relative;">
+            <div style="width: 100%; margin-top: -18px; margin-bottom: 7px; position: relative;">
                 <img src="data:image/png;base64,{header_image}" style="width: 100%; height: auto; border-radius: 4px; z-index:-1;">
                 <div style="position: absolute; bottom: 0px; right: 0px; background-color: rgba(0, 0, 0, 0.8); color: white; padding: 2px 4px; border-radius: 4px 0px 4px 0px; font-size: 0.6rem; font-weight: 700;">
                     {stats_text}
@@ -922,7 +922,7 @@ def create_deck_section(section_type):
             caption_text = "Shows meta share percentages"
         
         st.caption(f"{config['description']}. {caption_text}")
-    st.markdown(f"""<hr style='margin-bottom:40px; border: 0.5px solid rgba(137, 148, 166, 0.3); margin-top:0px;'>""", unsafe_allow_html=True)        
+    #st.markdown(f"""<hr style='margin-bottom:40px; border: 0.5px solid rgba(137, 148, 166, 0.3); margin-top:0px;'>""", unsafe_allow_html=True)        
      
 
 def analyze_counter_matchups(selected_deck_names, config):
