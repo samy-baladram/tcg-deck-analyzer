663c7abac726b9be4e7f0e4c0572969842ab7e26
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-06-08 12:28:16 +0900

    Update display_tabs.py

--- display_tabs.py
+++ display_tabs.py
@@ -43,8 +43,8 @@ def display_deck_header(deck_info, results):
         # Simplified centered layout
         header_content = f"""
         <link href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@1,900&display=swap" rel="stylesheet">
-        <h1 style="margin: 0; font-family: 'Nunito', sans-serif; font-weight: 900; font-style: italic; line-height: 1; word-wrap: break-word;">{format_deck_name(deck_info['deck_name'])}</h1>
         <div style="display: flex; flex-wrap: wrap; justify-content: center; align-items: center; gap: 0rem; margin: 0.5rem 0 0.5rem 0; text-align: center;">
+            <h1 style="margin: 0; font-family: 'Nunito', sans-serif; font-weight: 900; font-style: italic; line-height: 1; word-wrap: break-word;">{format_deck_name(deck_info['deck_name'])}</h1>
             <div>
                 <img src="data:image/png;base64,{header_image}" style="max-width: {deck_image_max_width}; width: 100%; height: auto; border: 3px solid #57585F;border-radius: 10px;">
             </div>
