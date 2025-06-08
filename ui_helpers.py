0a9ec2608164b7be8aa5a03026f819ea53d152e3
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-06-08 15:46:33 +0900

    Update ui_helpers.py

--- ui_helpers.py
+++ ui_helpers.py
@@ -824,7 +824,6 @@ def create_deck_section(section_type):
         banner_base64 = get_cached_banner_image(config['banner_path'])
         if banner_base64:
             st.markdown(f"""<div style="width:100%; text-align:center; ">
-                <hr style='margin-bottom:10px; border: 0.5px solid rgba(137, 148, 166, 0.3); margin-top:0px;'>
                 <img src="data:image/png;base64,{banner_base64}" style="width:100%; max-width:350px; margin-bottom:-2px;">
             </div>
             """, unsafe_allow_html=True)
@@ -906,6 +905,7 @@ def create_deck_section(section_type):
             caption_text = "Shows meta share percentages"
         
         st.caption(f"{config['description']}. {caption_text}")
+    st.markdown(f"""<hr style='margin-bottom:10px; border: 0.5px solid rgba(137, 148, 166, 0.3); margin-top:0px;'>""", unsafe_allow_html=True)        
         
 def display_section_disclaimer(config):
     """Display section-specific disclaimer text"""
