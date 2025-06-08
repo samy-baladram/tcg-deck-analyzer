4d6768344faedf22df8fd8bb076a968e12b36106
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-06-08 11:51:38 +0900

    Update ui_helpers.py

--- ui_helpers.py
+++ ui_helpers.py
@@ -800,7 +800,7 @@ def render_unified_deck_in_sidebar(deck, section_config, rank=None, expanded=Fal
         header_image = get_header_image_cached(deck['deck_name'], deck['set'])
         if header_image:
             st.markdown(f"""
-            <div style="width: 100%; margin-top: -22px; margin-bottom: 10px; position: relative;">
+            <div style="width: 100%; margin-top: -20px; margin-bottom: 10px; position: relative;">
                 <img src="data:image/png;base64,{header_image}" style="width: 100%; height: auto; border-radius: 4px; z-index:-1;">
                 <div style="position: absolute; bottom: 0px; right: 0px; background-color: rgba(0, 0, 0, 0.8); color: white; padding: 2px 4px; border-radius: 4px 0px 4px 0px; font-size: 0.6rem; font-weight: 700;">
                     {stats_text}
@@ -871,7 +871,7 @@ def create_deck_section(section_type):
         
     if header_image:
         st.markdown(f"""
-        <div style="width: 100%; margin-top: -22px; margin-bottom: 10px; position: relative;">
+        <div style="width: 100%; margin-top: -20px; margin-bottom: 10px; position: relative;">
             <img src="data:image/png;base64,{header_image}" style="width: 100%; height: auto; border-radius: 4px; z-index:-2;">
             <div style="position: absolute; bottom: 0px; right: 0px; background-color: rgba(0, 0, 0, 0.8); color: white; padding: 2px 4px; border-radius: 4px 0px 4px 0px; font-size: 0.6rem; font-weight: 700;">
                 {stats_text}
