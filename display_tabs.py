2efd8fe15ef46a4ef00214b56d1a121672a63c10
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-06-09 23:00:43 +0900

    Update display_tabs.py

--- display_tabs.py
+++ display_tabs.py
@@ -1118,7 +1118,7 @@ def display_matchup_bar_chart(deck_name, set_name, working_df):
             showgrid=False,
             showline=False,
             zeroline=False,
-            range=[0, 32]
+            range=[0, 29]
         ),
         
         # Remove legend and other elements
