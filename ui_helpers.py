4b574688c2e009862d19a682cb77b2bcd35f110c
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-06-22 13:26:33 +0900

    Update ui_helpers.py

--- ui_helpers.py
+++ ui_helpers.py
@@ -264,8 +264,8 @@ def display_banner(img_path, max_width=600):
     img_base64 = get_cached_banner_image(img_path)
     
     if img_base64:
-        st.markdown(f"""<div style="display: flex; justify-content: center; width: 100%;">
-            <img src="data:image/png;base64,{img_base64}" style="width: 100%; max-width: {max_width}px;  height: auto; margin-top:-70px; margin-bottom:-5px; ">
+        st.markdown(f"""<div style="display: flex; justify-content: center; width: 100%; margin-top:10px; margin-bottom:0px;">
+            <img src="data:image/png;base64,{img_base64}" style="width: 100%; max-width: {max_width}px;  height: auto; margin-top:-70px;">
         </div>
         """, unsafe_allow_html=True)
 
