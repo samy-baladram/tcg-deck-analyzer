2cdeb075fe4953d415709588611ef63b435d021a
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-06-26 11:31:06 +0900

    Update meta_table.py

--- meta_table.py
+++ meta_table.py
@@ -596,7 +596,7 @@ def display_meta_overview_table():
             styled_df,
             column_config=column_config,
             hide_index=True,
-            height=1000,
+            height=1200,
             use_container_width=True
         )
         
