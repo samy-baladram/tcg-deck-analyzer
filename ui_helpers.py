62eb251abb96134d18d31a07873e9cec787faf91
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-06-28 12:52:57 +0900

    Update ui_helpers.py

--- ui_helpers.py
+++ ui_helpers.py
@@ -928,7 +928,7 @@ def render_unified_deck_in_sidebar(deck, section_config, rank=None, expanded=Fal
             st.markdown(f"""
             <div style="width: 100%; margin-top: -16px; margin-bottom: 7px; position: relative;">
                 <img src="data:image/png;base64,{header_image}" style="width: 100%; height: auto; border-radius: 4px; z-index:-1;">
-                <div style="position: absolute; bottom: 0px; right: 0px; background-color: rgba(38, 39, 48, 0.75); color: white; padding: 2px 4px; border-radius: 4px 0px 4px 0px; font-size: 0.7rem; font-weight: 500;">
+                <div style="position: absolute; bottom: 0px; right: 0px; background-color: rgba(38, 39, 48, 0.75); color: lightcyan; padding: 2px 4px; border-radius: 4px 0px 4px 0px; font-size: 0.7rem; font-weight: 500;">
                     {stats_text}
                 </div>
             </div>
