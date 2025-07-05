694f84e445162d16b5f4e9519da7b4446d2608eb
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-07-05 18:38:43 +0900

    Update display_tabs.py

--- display_tabs.py
+++ display_tabs.py
@@ -26,7 +26,7 @@ def display_deck_header(deck_info, results):
     if header_image:
         # Simple centered deck image
         header_content = f"""
-        <div style="display: flex; justify-content: center; align-items: center; margin: 0.5rem 0rem -3rem 0rem; text-align: center;">
+        <div style="display: flex; justify-content: center; align-items: center; margin: 1rem 0rem -3rem 0rem; text-align: center;">
             <div style="min-width: 200px;">
                 <img src="data:image/png;base64,{header_image}" style="max-width: 500px; margin-top: -17rem; margin-bottom: 0rem; width: 80%; height: auto; border: 0px solid #57585F; border-radius: 10px;">
             </div>
