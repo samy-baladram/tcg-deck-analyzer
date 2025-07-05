4e70ca281765701d9237a8eeb30b19e5ebc72fd4
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-07-05 18:45:08 +0900

    Update display_tabs.py

--- display_tabs.py
+++ display_tabs.py
@@ -25,12 +25,16 @@ def display_deck_header(deck_info, results):
     
     if header_image:
         # Simple centered deck image
-        header_content = f"""
-        <div style="display: flex; justify-content: center; align-items: flex-end; margin: 0rem 0rem -3rem 0rem; text-align: center;">
-            <div style="min-width: 200px;">
+        # header_content = f"""
+        # <div style="display: flex; justify-content: center; align-items: flex-end; margin: 0rem 0rem -3rem 0rem; text-align: center;">
+        #     <div style="min-width: 200px;">
+        #         <img src="data:image/png;base64,{header_image}" style="max-width: 500px; margin-top: -17rem; margin-bottom: 0rem; width: 80%; height: auto; border: 0px solid #57585F; border-radius: 6px; object-position: bottom;">
+        #     </div>
+        # </div>"""
+       header_content = f"""
+            <div style="text-align:center; margin: 0rem 0rem -3rem 0rem;">
                 <img src="data:image/png;base64,{header_image}" style="max-width: 500px; margin-top: -17rem; margin-bottom: 0rem; width: 80%; height: auto; border: 0px solid #57585F; border-radius: 6px; object-position: bottom;">
-            </div>
-        </div>"""
+            </div>"""
         
         st.markdown(header_content, unsafe_allow_html=True)
             
