bb9145f268587756c6cd350bac3a1068c002e1b0
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-07-05 22:25:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -279,7 +279,7 @@ ui_helpers.display_banner("title_banner.png")
 
 # Create deck selector AFTER initialization
 st.markdown('''
-<div style="height: 80px;"></div>
+<div style="height: 75px;"></div>
 ''', unsafe_allow_html=True)
 selected_option = ui_helpers.create_deck_selector()
 
