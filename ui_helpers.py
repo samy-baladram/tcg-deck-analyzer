384bfb095656ca14da323c8415bebd066b27d335
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-07-05 22:33:04 +0900

    Update ui_helpers.py

--- ui_helpers.py
+++ ui_helpers.py
@@ -253,7 +253,7 @@ def display_banner(img_path, max_width=500):
     
     if img_base64:
         st.markdown(f"""<div style="display: flex; justify-content: center; width: 100%; margin-top:10px; margin-bottom:0px;">
-            <img src="data:image/png;base64,{img_base64}" style="width: 100%; max-width: {max_width}px;  height: auto; margin-top:-70px;">
+            <img src="data:image/png;base64,{img_base64}" style="width: 100%; max-width: {max_width}px;  height: auto; margin-top:-75px;">
         </div>
         """, unsafe_allow_html=True)
 
