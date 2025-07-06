9159237164736f5675f6f67af9ee67e1744fade5
Samy Baladram <62135321+samy-baladram@users.noreply.github.com>
2025-07-06 15:37:59 +0900

    Update config.py

--- config.py
+++ config.py
@@ -99,7 +99,7 @@ PLOTLY_CONFIG = {
 # Image processing settings
 IMAGE_CROP_BOX = {
     'left': 0.05,
-    'top': 0.09,
+    'top': 0.08,
     #'top': 0.115,
     'right': 0.95,
     'bottom': 0.47
